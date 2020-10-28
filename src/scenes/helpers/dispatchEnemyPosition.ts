f16c82ae430e9a0bf7c82ca43d1c8d513b52d47e
Jean-Marie <contact@cartlamy.com>
2020-10-28 11:55:05 +0100

    feat(enemy): dispatch depending on player position

---
+++ src/scenes/helpers/dispatchEnemyPosition.ts
@@ -0,0 +1,27 @@
+import { IEnemyPosition } from '../../types';
+
+export default function(
+  enemyAvailablePositionMap: IEnemyPosition[],
+  position: { x: number; y: number },
+  direction: 'right' | 'top'
+) {
+  let enemy = null;
+  const max = enemyAvailablePositionMap.length - 1;
+
+  while (!enemy) {
+    const index = Phaser.Math.RND.integerInRange(0, max);
+    const selected = enemyAvailablePositionMap[index];
+
+    if (direction === 'top') {
+      if (position.y > selected.y && position.y - selected.y < 3000) {
+        enemy = selected;
+      }
+    } else if (direction === 'right') {
+      if (position.x < selected.x && selected.x - position.x < 2000) {
+        enemy = selected;
+      }
+    }
+  }
+    console.log(enemy, position)
+  return enemy;
+}
